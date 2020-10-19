084ccad7e6402b28d447270edbaf116243ed8391
Charlie Sakamaki <csakamaki@revacomm.com>
2020-10-18 20:55:06 -1000

    Change version

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React 3
-          </a>
+          <h1 className="App-title">Fib Calculator version 2</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
